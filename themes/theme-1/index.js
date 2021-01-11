28c82d4ba69765422ce202c4b251dadcd675c7e8
adrian-haynes-postclick <adrian.haynes@postclick.com>
2021-01-11 15:53:45 -0600

    add core js polyfill plugin

--- themes/theme-1/index.js
+++ themes/theme-1/index.js
@@ -1,3 +1,16 @@
+/**
+ * Polyfills
+ * NOTE: only include necessary polyfills 
+ * More info: https://www.npmjs.com/package/core-js
+ */
+// import 'core-js/features/array/from';
+// import 'core-js/features/array/flat';
+// import 'core-js/features/set';       
+// import 'core-js/features/promise';   
+
+/**
+ * CSS & JS files
+ */
 import './src/css/header.css';
 import './src/css/styles.css';
 import './src/js/script.js';
